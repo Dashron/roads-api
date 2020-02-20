8d645a006ff76003bdab4ef3ea6db5f77fcdee32
Aaron Hedges <aaron@dashron.com>
2020-02-19 22:13:46 -0500

    add missing file from previous commit

---
+++ src/tests/data/postCollectionRepresentation.ts
@@ -0,0 +1,38 @@
+"use strict";
+
+import JSONRepresentation from "../../Representation/jsonRepresentation";
+import postRepresentation from './postRepresentation';
+
+//module.exports = function (itemRepresentation, resolveArrayItems) {
+export default class CollectionRepresentation extends JSONRepresentation {
+    constructor (action: string) {
+        super();
+
+        this.init({
+            "type": "object",
+            "properties": {
+                "data": {
+                    "type": "array",
+                    // I would love this class to be more generic, and have items and resolveArrayItems be passed in, but right now it doesn't work.
+                    "items": new postRepresentation(action),
+                    "resolveArrayItems": (models: any) => {
+                        return models.posts;
+                    }
+                },
+                "perPage": {
+                    "type": "number",
+                    "resolve": (models: {perPage: number}) => {
+                        return models.perPage;
+                    }
+                },
+                "page": {
+                    "type": "number",
+                    "resolve": (models: {page: number}) => {
+                        return models.page;
+                    }
+                }
+            },
+            "additionalProperties": false
+        });
+    }
+};
